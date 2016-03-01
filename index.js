84d1a6b42e2ef6b74c7f73dbcaa4f4a846ca4ff3
James Monger <jameskmonger@hotmail.co.uk>
2016-03-01 23:43:16 +0000

    multi test runner complete

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+'use strict';
+
 const Benchmark = require('benchmark');
 const forkeys = require('forkeys');
 const forkeys_c = require('forkeys-compat');
+
+const SUITE_COUNT = 20;
 
 const testObject = {
     first_key: 'blablabla',
@@ -17,29 +21,38 @@ const testObject = {
     ]
 };
 
-var results = [];
+let results = {
+    'forkeys(a, b)': [],
+    'forkeys-compat(a, b)': [],
+    'Object.keys(a).forEach(b)': []
+};
 
-var suite = new Benchmark.Suite;
-
-suite
-    .add('forkeys(a, b)', function() {
-        forkeys(testObject, function(key) {
-            // dummy functionality here
+for (let i = 0; i < SUITE_COUNT; i++) {
+    (new Benchmark.Suite)
+        .add('forkeys(a, b)', function() {
+            forkeys(testObject, function(key) {
+                // dummy functionality here
+            });
+        })
+        .add('forkeys-compat(a, b)', function() {
+            forkeys_c(testObject, function(key) {
+                // dummy functionality here
+            });
+        })
+        .add('Object.keys(a).forEach(b)', function() {
+            Object.keys(testObject).forEach(function(key) {
+                // dummy functionality here
+            });
+        })
+        .on('cycle', function(event) {
+            results[event.target.name].push(event.target.hz);
+        })
+        .on('complete', function() {
+            if (i === SUITE_COUNT) {
+                console.log(results);
+            }
+        })
+        .run({
+            async: true
         });
-    })
-    .add('forkeys-compat(a, b)', function() {
-        forkeys_c(testObject, function(key) {
-            // dummy functionality here
-        });
-    })
-    .add('Object.keys(a).forEach(b)', function() {
-        Object.keys(testObject).forEach(function(key) {
-            // dummy functionality here
-        });
-    })
-    .on('cycle', function(event) {
-        console.log(event.target.name + ": " + event.target.hz);
-    })
-    .run({
-        async: true
-    });
+}
