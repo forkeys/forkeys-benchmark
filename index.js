0b3ab4712c2593839688eaedddd7001d948a678c
James Monger <jameskmonger@hotmail.co.uk>
2016-03-01 23:25:53 +0000

    Remove cycle listener

--- index.js
+++ index.js
@@ -38,7 +38,4 @@ suite
     .on('cycle', function(event) {
         console.log(String(event.target));
     })
-    .on('complete', function() {
-        console.log('Fastest is ' + this.filter('fastest').map('name'));
-    })
     .run();
